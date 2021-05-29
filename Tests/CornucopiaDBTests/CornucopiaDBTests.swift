2d1e8c9ecab2b06fe9a3d9e4b41c30b0b68ed76e
Dr. Michael Lauer <mickey@vanille-media.de>
2021-05-29 12:58:01 +0200

    tests++

--- Tests/CornucopiaDBTests/CornucopiaDBTests.swift
+++ Tests/CornucopiaDBTests/CornucopiaDBTests.swift
@@ -1,19 +1,44 @@
 import XCTest
 @testable import CornucopiaDB
 
+import Foundation
+
 final class CornucopiaDBTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        let db = Database()
 
+    func testCreate() {
 
-
+        let db = CornucopiaDB.Database(name: "\(UUID())")
         XCTAssertEqual(true, true)
     }
 
+    func testCodableCollection() {
+
+        struct CodableStruct: Codable, Equatable {
+            let key: String
+            let i: Int
+            let s: String
+            let d: Date
+        }
+
+        let db = CornucopiaDB.Database(name: "\(UUID())")
+        let coll = db.codableCollection(keyed: \CodableStruct.key)
+        let item = CodableStruct(key: "foo", i: 42, s: "bar", d: Date())
+        coll.persist(item: item)
+
+        let decodedItem = coll.item(for: "foo")
+        XCTAssertEqual(decodedItem, item)
+
+
+
+
+
+
+        
+
+    }
+
     static var allTests = [
-        ("testExample", testExample),
+        ("testCreate", testCreate),
+        ("testCodableCollection", testCodableCollection),
     ]
 }
